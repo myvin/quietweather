f7e230d3580febcbd931edf0ba6539678461b62d
myvin <851399101@qq.com>
2018-10-09 18:26:36 +0800

    add cloud config

--- cloudfunction/getBroadcast/index.js
+++ cloudfunction/getBroadcast/index.js
@@ -1,7 +1,7 @@
 const cloud = require('wx-server-sdk')
 cloud.init()
 const db = cloud.database({
-  env: 'product-d4c723',
+  env: 'envid',
 })
 exports.main = async (event, context) => {
   return db.collection('broadcast')
