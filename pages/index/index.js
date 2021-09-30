ee589d8e58561e4f4a2773601275bac5bf35c9ae
myvin <851399101@qq.com>
2021-09-30 15:04:37 +0800

    add constant file

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,3 +1,5 @@
+import { BackgroundImageList, EnvironmentVars, LifeStyles } from '../../constant';
+
 let utils = require('../../utils/utils')
 let globalData = getApp().globalData
 const key = globalData.key
@@ -9,40 +11,11 @@ Page({
     cityDatas: {},
     hourlyDatas: [],
     weatherIconUrl: globalData.weatherIconUrl,
-    detailsDic: {
+    EnvironmentVars: {
       key: ['tmp', 'fl', 'hum', 'pcpn', 'wind_dir', 'wind_deg', 'wind_sc', 'wind_spd', 'vis', 'pres', 'cloud', ''],
-      val: {
-        tmp: '温度(℃)',
-        fl: '体感温度(℃)',
-        hum: '相对湿度(%)',
-        pcpn: '降水量(mm)',
-        wind_dir: '风向',
-        wind_deg: '风向角度(deg)',
-        wind_sc: '风力(级)',
-        wind_spd: '风速(mk/h)',
-        vis: '能见度(km)',
-        pres: '气压(mb)',
-        cloud: '云量',
-      },
+      val: EnvironmentVars,
     },
-    lifestyles: {
-      'comf': '舒适度指数',
-      'cw': '洗车指数',
-      'drsg': '穿衣指数',
-      'flu': '感冒指数',
-      'sport': '运动指数',
-      'trav': '旅游指数',
-      'uv': '紫外线指数',
-      'air': '空气污染扩散条件指数',
-      'ac': '空调开启指数',
-      'ag': '过敏指数',
-      'gl': '太阳镜指数',
-      'mu': '化妆指数',
-      'airc': '晾晒指数',
-      'ptfc': '交通指数',
-      'fsh': '钓鱼指数',
-      'spi': '防晒指数',
-    },
+    lifestyles: LifeStyles,
     // 用来清空 input
     searchText: '',
     // 是否已经弹出
@@ -56,40 +29,7 @@ Page({
     // 需要查询的城市
     searchCity: '',
     setting: {},
-    bcgImgList: [
-      {
-        src: '/img/beach-bird-birds-235787.jpg',
-        topColor: '#393836'
-      },
-      {
-        src: '/img/clouds-forest-idyllic-417102.jpg',
-        topColor: '#0085e5'
-      },
-      {
-        src: '/img/backlit-dawn-dusk-327466.jpg',
-        topColor: '#2d2225'
-      },
-      {
-        src: '/img/accomplishment-adventure-clear-sky-585825.jpg',
-        topColor: '#004a89'
-      },
-      {
-        src: '/img/fog-himalayas-landscape-38326.jpg',
-        topColor: '#b8bab9'
-      },
-      {
-        src: '/img/asphalt-blue-sky-clouds-490411.jpg',
-        topColor: '#009ffe'
-      },
-      {
-        src: '/img/aerial-climate-cold-296559.jpg',
-        topColor: '#d6d1e6'
-      },
-      {
-        src: '/img/beautiful-cold-dawn-547115.jpg',
-        topColor: '#ffa5bc'
-      }
-    ],
+    bcgImgList: BackgroundImageList,
     bcgImgIndex: 0,
     bcgImg: '',
     bcgImgAreaShow: false,
